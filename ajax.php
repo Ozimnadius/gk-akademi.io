42780e083d755252c9a7f95a75c7b1c5730c6741
Seregin Mikle <mikhail-seregin@yandex.ru>
2018-10-10 12:38:51 +0300

    js Forms

--- ajax.php
+++ ajax.php
@@ -7,7 +7,14 @@ switch ($action) {
     case 'callorderSubmit':
         echo json_encode(array(
             'status' => true,
-            'html' => '<div class="callorder__success">Спасибо мы скоро с Вами свяжемся.</div>'
+            'html' => '<div class="form__success">Спасибо мы скоро с Вами свяжемся. <div class="form__success-close"><svg class="form__success-svg"><use xlink:href="images/icons/sprite.svg#close"></use> </svg></div></div>'
+        ));
+        exit();
+        break;
+    case 'modalOpen':
+        echo json_encode(array(
+            'status' => true,
+            'html' => modalContent($_POST['id'])
         ));
         exit();
         break;
@@ -17,4 +24,38 @@ switch ($action) {
         ));
         exit();
         break;
+}
+
+function modalContent($id)
+{
+    ob_start();
+    ?>
+    <div class="modal__title">
+        Учебным заведениям
+    </div>
+    <div class="modal__img">
+        <img src="images/content/modal.png" alt="Учебным заведениям" class="modal__pic">
+    </div>
+
+    <ul class="modal__list">
+        <li class="modal__item">Оборудование для организации учебного процесса</li>
+        <li class="modal__item">Хозяйственные, моющие и дезинфицирующие средства</li>
+        <li class="modal__item">Товары для делопроизводства </li>
+        <li class="modal__item">Компьютеры и комплектующие</li>
+        <li class="modal__item">Системы архивации и хранения</li>
+        <li class="modal__item">Презентационное и демонстрационное оборудование</li>
+        <li class="modal__item">Бумага и писчебумажная продукция</li>
+        <li class="modal__item">Оргтехника и расходные материалы</li>
+        <li class="modal__item">Спецодежда</li>
+        <li class="modal__item">Вывески, таблички, информационные доски</li>
+        <li class="modal__item">Средства индивидуальной защиты</li>
+    </ul>
+
+    <a href="#" class="modal__link jsCallorderOpen">
+        Уточнить подробности
+    </a>
+    <?
+    $html = ob_get_contents();
+    ob_end_clean();
+    return $html;
 }
--- js/main.js
+++ js/main.js
@@ -1,7 +1,84 @@
-// $(function () {
+$(function () {
 
+    /*CALLORDER*/
+    const callorder = $('.callorder');
+    $('form').on('submit', function (e) {
+        e.preventDefault();
 
-    const slider = new Swiper ('.slider__container', {
+        let form = $(this),
+            data = form.serialize();
+
+        $.ajax({
+            dataType: "json",
+            type: "POST",
+            url: 'ajax.php',
+            data: data,
+            success: function (result) {
+
+                if (result.status) {
+                    form.append(result.html);
+                } else {
+                    alert('Что-то пошло не так, попробуйте еще раз!!!');
+                }
+            },
+            error: function (result) {
+                alert('Что-то пошло не так, попробуйте еще раз!!!');
+            }
+        });
+    });
+
+    $('body').on('click', '.form__success-close', function (e) {
+        $('.form__success').remove();
+    });
+
+    $('body').on('click', '.jsCallorderOpen', function (e) {
+        e.preventDefault();
+        modal.removeClass('active');
+        callorder.addClass('active');
+    });
+    $('body').on('click', '.jsCallorderClose', function (e) {
+        e.preventDefault();
+        callorder.removeClass('active');
+    });
+    /*END CALLORDER*/
+
+    /*MODAL*/
+    const modal = $('.modal');
+    $('body').on('click', '.jsModalOpen', function (e) {
+        e.preventDefault();
+        let modalContent = modal.find('.modal__content'),
+        data = {
+            id: parseInt($(this).data('id')),
+            action: 'modalOpen'
+        };
+
+        $.ajax({
+            dataType: "json",
+            type: "POST",
+            url: 'ajax.php',
+            data: data,
+            success: function (result) {
+                if (result.status) {
+                    modalContent.html(result.html);
+                    modal.addClass('active');
+                    callorder.removeClass('active');
+                } else {
+                    alert('Что-то пошло не так, попробуйте еще раз!!!');
+                }
+            },
+            error: function (result) {
+                alert('Что-то пошло не так, попробуйте еще раз!!!');
+            }
+        });
+    });
+    $('body').on('click', '.jsModalClose', function (e) {
+        e.preventDefault();
+        modal.removeClass('active');
+    });
+    /*END MODAL*/
+
+    /*SWIPER*/
+    const slider = new Swiper('.slider__container', {
         // Optional parameters
         // loop: true,
         speed: 700,
@@ -14,17 +91,17 @@
         pagination: {
             el: '.swiper-pagination',
             type: 'fraction',
-            formatFractionTotal: function(number) {
-               if (number <10) {
-                   return '0'+number;
-               } else  {
-                   return number;
-               }
+            formatFractionTotal: function (number) {
+                if (number < 10) {
+                    return '0' + number;
+                } else {
+                    return number;
+                }
             },
-            formatFractionCurrent: function(number) {
-                if (number <10) {
-                    return '0'+number;
-                } else  {
+            formatFractionCurrent: function (number) {
+                if (number < 10) {
+                    return '0' + number;
+                } else {
                     return number;
                 }
             },
@@ -36,4 +113,6 @@
         },
 
     });
-// });
+    /*END SWIPER*/
+
+});
